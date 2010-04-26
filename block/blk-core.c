c344f43ea37346cb43f272643505296d40cf618c
Steve Sakoman <steve@sakoman.com>
2010-04-26 11:17:26 -0700

    MTD: silence ecc errors on mtdblock0
    mtdblock0 is the x-load partition, which uses hw ecc
    this confuses linux, which uses sw ecc
    this patch silences ecc error messages when linux peeks into mtdblock0
    * not for upstream submission *

--- block/blk-core.c
+++ block/blk-core.c
@@ -2028,9 +2028,10 @@ bool blk_update_request(struct request *req, int error, unsigned int nr_bytes)
 
 	if (error && req->cmd_type == REQ_TYPE_FS &&
 	    !(req->cmd_flags & REQ_QUIET)) {
-		printk(KERN_ERR "end_request: I/O error, dev %s, sector %llu\n",
-				req->rq_disk ? req->rq_disk->disk_name : "?",
-				(unsigned long long)blk_rq_pos(req));
+		if (req->rq_disk && (strcmp(req->rq_disk->disk_name, "mtdblock0") != 0))
+				printk(KERN_ERR "end_request: I/O error, dev %s, sector %llu\n",
+					req->rq_disk ? req->rq_disk->disk_name : "?",
+					(unsigned long long)blk_rq_pos(req));
 	}
 
 	blk_account_io_completion(req, nr_bytes);
