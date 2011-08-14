95acc4544bb785082fa9abe50660d717564f5b85
Bjoern Krombholz <b.krombholz@pironex.de>
2011-08-14 04:38:59 +0200

    pia35x: fix serial initialization for console output
    The serial console must be initialized correctly after the kernel was
    uncompressed, otherwise it will die silently.

--- arch/arm/plat-omap/include/plat/uncompress.h
+++ arch/arm/plat-omap/include/plat/uncompress.h
@@ -150,6 +150,7 @@ static inline void __arch_decomp_setup(unsigned long arch_id)
 		DEBUG_LL_OMAP3(3, cm_t35);
 		DEBUG_LL_OMAP3(3, cm_t3517);
 		DEBUG_LL_OMAP3(3, craneboard);
+		DEBUG_LL_OMAP3(3, pia_am35x);
 		DEBUG_LL_OMAP3(3, devkit8000);
 		DEBUG_LL_OMAP3(3, igep0020);
 		DEBUG_LL_OMAP3(3, igep0030);
