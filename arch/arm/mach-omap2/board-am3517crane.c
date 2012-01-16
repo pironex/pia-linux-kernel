a82978c075ceccf19bd20f87f480a260b1848c64
Björn Krombholz <b.krombholz@pironex.de>
2012-01-16 17:31:37 +0100

    pia35x: first part of port to 2.6.37
    - initialisation of MMC1
    - initialisation of fixed_regulator for MMC1
    - TODO complete regulator TPS
    
    Conflicts:
    
    	arch/arm/mach-omap2/board-am3517crane.c
    
    Signed-off-by: Björn Krombholz <b.krombholz@pironex.de>

--- arch/arm/mach-omap2/board-am3517crane.c
+++ arch/arm/mach-omap2/board-am3517crane.c
@@ -20,6 +20,8 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/gpio.h>
+#include <linux/mmc/host.h>
+#include <linux/regulator/machine.h>
 
 #include <mach/hardware.h>
 #include <asm/mach-types.h>
@@ -32,6 +34,7 @@
 
 #include "mux.h"
 #include "control.h"
+#include "hsmmc.h"
 
 #define GPIO_USB_POWER		35
 #define GPIO_USB_NRESET		38
@@ -58,17 +61,93 @@ static struct usbhs_omap_board_data usbhs_bdata __initdata = {
 	.reset_gpio_port[2]  = -EINVAL
 };
 
+/*
+ * MMC
+ */
+#include <linux/regulator/fixed.h>
+
+static struct regulator_consumer_supply pia3517_vmmc1_supply = {
+	.supply         = "vmmc",
+	.dev_name       = "mmci-omap-hs.0", /* bind to our MMC1 device */
+};
+
+static struct regulator_init_data pia3517_vmmc1_data = {
+	.constraints = {
+			.min_uV           = 1850000,
+			.max_uV           = 3150000,
+			.valid_modes_mask = REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+			.valid_ops_mask   = REGULATOR_CHANGE_VOLTAGE
+					| REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+	.num_consumer_supplies = 1,
+	.consumer_supplies = &pia3517_vmmc1_supply,
+};
+
+static struct fixed_voltage_config pia3517_vmmc1_config = {
+	.supply_name = "vmmc1",
+	.microvolts = 1800000,  /* 1.8V */
+	//.gpio = OMAP_BEAGLE_WLAN_EN_GPIO,
+	.startup_delay = 70000, /* 70ms */
+	.enable_high = 1,
+	.enabled_at_boot = 1,
+	.init_data = &pia3517_vmmc1_data,
+};
+
+static struct platform_device pia3517_vmmc1_device = {
+	.name           = "reg-fixed-voltage",
+	.id             = 1,
+	.dev = {
+		.platform_data = &pia3517_vmmc1_config,
+	},
+};
+
+static struct omap2_hsmmc_info mmc[] = {
+	/* first MMC port used for system MMC modules */
+	{
+		.mmc            = 1,
+		.caps           = MMC_CAP_4_BIT_DATA,
+		.gpio_cd        = 41,
+		.gpio_wp        = 40,
+	},
+#if defined(CONFIG_WL1271) || defined (CONFIG_WL1271_MODULEx)
+	{
+		.name		= "wl1271",
+		.mmc		= 2,
+		.caps		= MMC_CAP_4_BIT_DATA | MMC_CAP_POWER_OFF_CARD,
+		.gpio_wp	= -EINVAL,
+		.gpio_cd	= -EINVAL,
+		.nonremovable	= true,
+	},
+#endif /* CONFIG_WL12XX_PLATFORM_DATA */
+	{}	/* Terminator */
+};
+
+static void pia3517_mmc_init(void)
+{
+	printk(KERN_INFO "piA-am35x: registering VMMC1 platform device\n");
+	platform_device_register(&pia3517_vmmc1_device);
+	/* handling of different MMC2 expansions here */
+	omap2_hsmmc_init(mmc);
+	/* link regulators to MMC adapters */
+	//xx_vmmc1_supply.dev = mmc[0].dev;
+	//xx_vsim_supply.dev = mmc[0].dev;
+
+}
+
 static void __init am3517_crane_init(void)
 {
-	int ret;
-
+	/* additional pin muxing */
 	omap3_mux_init(board_mux, OMAP_PACKAGE_CBB);
+	/* initialize uarts */
 	omap_serial_init();
 	omap_sdrc_init(NULL, NULL);
 
 	omap_board_config = am3517_crane_config;
 	omap_board_config_size = ARRAY_SIZE(am3517_crane_config);
 
+#ifdef NOT_USED
 	/* Configure GPIO for EHCI port */
 	if (omap_mux_init_gpio(GPIO_USB_NRESET, OMAP_PIN_OUTPUT)) {
 		pr_err("Can not configure mux for GPIO_USB_NRESET %d\n",
@@ -90,6 +169,9 @@ static void __init am3517_crane_init(void)
 	}
 
 	usbhs_init(&usbhs_bdata);
+#endif
+
+	pia3517_mmc_init();
 }
 
 MACHINE_START(CRANEBOARD, "AM3517/05 CRANEBOARD")
