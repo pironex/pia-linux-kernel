684a638e2f4c80c55f44d5e52a3e12f843ea4e90
Björn Krombholz <b.krombholz@pironex.de>
2012-05-08 14:37:24 +0200

    pia35x: add board support for DEM 480272D based display expansion
    Signed-off-by: Björn Krombholz <b.krombholz@pironex.de>

--- arch/arm/mach-omap2/board-am35xpia.c
+++ arch/arm/mach-omap2/board-am35xpia.c
@@ -119,7 +119,9 @@ static int __init pia35x_gsm_init(void)
 #define GPIO_LCDDVI_SWITCH 140 /* 1 = DVI, 0 = LCD */
 
 #if defined(CONFIG_PANEL_SHARP_LQ043T1DG01) || \
-		defined(CONFIG_PANEL_SHARP_LQ043T1DG01_MODULE)
+		defined(CONFIG_PANEL_SHARP_LQ043T1DG01_MODULE) || \
+		defined(CONFIG_PANEL_DEM_480272D) || \
+		defined(CONFIG_PANEL_DEM_480272D_MODULE)
 static int pia35x_lcd_enable(struct omap_dss_device *dssdev)
 {
 	gpio_set_value(GPIO_LCD_DISP, 1);
@@ -289,10 +291,13 @@ static void __init pia35x_touch_init(void)
 static void __init pia35x_display_init(void)
 {
 	int ret;
-	int use_lcd = 0;
+	int use_lcd = 1;
 
-	if (0 == strcmp(lcdboard_name, "pia_lcd"))
-		use_lcd = 1;
+	/* all lcd board names should start with "pia_lcd" */
+	if (0 != strncmp(lcdboard_name, "pia_lcd", 7))
+		use_lcd = 0;
+	else if (0 == strcmp(lcdboard_name, "pia_lcd_dem"))
+		pia35x_lcd_device.driver_name = "dem_480272d_panel";
 
 	if (0 == use_lcd)
 		pia35x_dss_data.default_device = &pia35x_dvi_device;
