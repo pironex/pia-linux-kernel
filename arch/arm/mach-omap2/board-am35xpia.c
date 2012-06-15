3044edadc4435da35ad540e7709e4b2add908d8c
Björn Krombholz <b.krombholz@pironex.de>
2012-06-15 12:30:21 +0200

    pia35x: handle different revisions of lcd expansions
    - we have 2 revisions of expansion boards using differnt I2C addresses
    
    Signed-off-by: Björn Krombholz <b.krombholz@pironex.de>

--- arch/arm/mach-omap2/board-am35xpia.c
+++ arch/arm/mach-omap2/board-am35xpia.c
@@ -328,14 +328,18 @@ static void __init pia35x_display_init(void)
 {
 	int ret;
 	int use_lcd = 1;
+	char *sub = 0;
+	int rev = 0;
 
+	sub = strrchr(lcdboard_name, '-');
+	if (sub != NULL)
+		rev = 0 - simple_strtol(sub, 0, 10);
 	/* all lcd board names should start with "pia_lcd" */
-	if (0 != strncmp(lcdboard_name, "pia_lcd", 7))
+	if (0 != strncmp(lcdboard_name, "pia_lcd", 7)) {
 		use_lcd = 0;
-	else if (0 == strcmp(lcdboard_name, "pia_lcd_dem")) {
+	} else if (0 == strncmp(lcdboard_name, "pia_lcd_dem", 11)) {
 		pia35x_lcd_device.driver_name = pia35x_dem_name;
-		pia35x_i2c3_tsc2007[0].addr = 0x48;
-	} else if (0 == strcmp(lcdboard_name, "pia_lcd_dt028")) {
+	} else if (0 == strncmp(lcdboard_name, "pia_lcd_dt028", 13)) {
 		//pia35x_dss_devices[0] = &pia35x_lcd_dt_device;
 		if ((ret = gpio_request_one(GPIO_DISP_RESET,
 			GPIOF_DIR_OUT | GPIOF_INIT_HIGH,"lcd-reset")) != 0) {
@@ -353,6 +357,10 @@ static void __init pia35x_display_init(void)
 			ARRAY_SIZE(pia35x_spi_dt_board_info));
 		pia35x_lcd_device.driver_name = pia35x_dt028_name;
 		pia35x_lcd_device.phy.dpi.data_lines = 18;
+	}
+
+	/* the tsc device address changed for revision 1 display expansions */
+	if (use_lcd && rev == 1) {
 		pia35x_i2c3_tsc2007[0].addr = 0x48;
 	}
 
